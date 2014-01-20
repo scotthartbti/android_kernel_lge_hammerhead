franciscofranco/hammerhead

usb: add working fast charge based on @faux123 work in this commit https://github.com/faux123/Nexus_5/commit/fb1a01502eda458e17120af4f6a1b297a9a679da
Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

drivers/power/bq24192_charger.c
@@ -30,6 +30,7 @@
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <linux/qpnp/qpnp-adc.h>
+#include <linux/fastchg.h>
 
 /* Register definitions */
 #define INPUT_SRC_CONT_REG              0X00
@@ -163,8 +164,8 @@ struct current_limit_entry {
 };
 
 static struct current_limit_entry adap_tbl[] = {
-	{1200, 1024},
-	{2000, 1536},
+	{1500, 1280},
+	{2000, 1696},
 };
 
 static int bq24192_step_down_detect_disable(struct bq24192_chip *chip);
@@ -316,6 +317,7 @@ static int bq24192_set_input_i_limit(struct bq24192_chip *chip, int ma)
 {
 	int i;
 	u8 temp;
+	int new_ma = ma;
 
 	if (ma < INPUT_CURRENT_LIMIT_MIN_MA
 			|| ma > INPUT_CURRENT_LIMIT_MAX_MA) {
@@ -333,19 +335,27 @@ static int bq24192_set_input_i_limit(struct bq24192_chip *chip, int ma)
 		i = 0;
 	}
 
-	temp = icl_ma_table[i].value;
+	if (force_fast_charge)
+	{
+		new_ma = 1200;
+		temp = icl_ma_table[4].value;
+	}
+	else
+	{
+		temp = icl_ma_table[i].value;
+	}
 
-	if (ma > chip->max_input_i_ma) {
-		chip->saved_input_i_ma = ma;
-		pr_debug("reject %d mA due to therm mitigation\n", ma);
+	if (new_ma > chip->max_input_i_ma) {
+		chip->saved_input_i_ma = new_ma;
+		pr_debug("reject %d mA due to therm mitigation\n", new_ma);
 		return 0;
 	}
 
 	if (!chip->therm_mitigation)
-		chip->saved_input_i_ma = ma;
+		chip->saved_input_i_ma = new_ma;
 
 	chip->therm_mitigation = false;
-	pr_debug("input current limit = %d setting 0x%02x\n", ma, temp);
+	pr_debug("input current limit = %d setting 0x%02x\n", new_ma, temp);
 	return bq24192_masked_write(chip->client, INPUT_SRC_CONT_REG,
 			INPUT_CURRENT_LIMIT_MASK, temp);
 }

scotthartbti/android_kernel_lge_hammerhead

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	drivers/power/bq24192_charger.c

drivers/power/bq24192_charger.c
@@ -30,6 +30,7 @@
 #include <linux/of_device.h>
 #include <linux/of_gpio.h>
 #include <linux/qpnp/qpnp-adc.h>
+#include <linux/fastchg.h>
 
 /* Register definitions */
 #define INPUT_SRC_CONT_REG              0X00
@@ -163,8 +164,8 @@ struct current_limit_entry {
 };
 
 static struct current_limit_entry adap_tbl[] = {
-	{1200, 1024},
-	{2000, 1536},
+	{1500, 1280},
+	{2000, 1696},
 };
 
 static int bq24192_step_down_detect_disable(struct bq24192_chip *chip);
@@ -316,6 +317,7 @@ static int bq24192_set_input_i_limit(struct bq24192_chip *chip, int ma)
 {
 	int i;
 	u8 temp;
+	int new_ma = ma;
 
 	if (ma < INPUT_CURRENT_LIMIT_MIN_MA
 			|| ma > INPUT_CURRENT_LIMIT_MAX_MA) {
@@ -333,19 +335,27 @@ static int bq24192_set_input_i_limit(struct bq24192_chip *chip, int ma)
 		i = 0;
 	}
 
-	temp = icl_ma_table[i].value;
+	if (force_fast_charge)
+	{
+		new_ma = 1200;
+		temp = icl_ma_table[4].value;
+	}
+	else
+	{
+		temp = icl_ma_table[i].value;
+	}
 
-	if (ma > chip->max_input_i_ma) {
-		chip->saved_input_i_ma = ma;
-		pr_info("reject %d mA due to therm mitigation\n", ma);
+	if (new_ma > chip->max_input_i_ma) {
+		chip->saved_input_i_ma = new_ma;
+		pr_info("reject %d mA due to therm mitigation\n", new_ma);
 		return 0;
 	}
 
 	if (!chip->therm_mitigation)
-		chip->saved_input_i_ma = ma;
+		chip->saved_input_i_ma = new_ma;
 
 	chip->therm_mitigation = false;
-	pr_info("input current limit = %d setting 0x%02x\n", ma, temp);
+	pr_info("input current limit = %d setting 0x%02x\n", new_ma, temp);
 	return bq24192_masked_write(chip->client, INPUT_SRC_CONT_REG,
 			INPUT_CURRENT_LIMIT_MASK, temp);
 }