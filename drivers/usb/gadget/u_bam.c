2fa0151e50816144820f0ad1cbd50e4c06306902
scott <scott@ubuntu.com>
2014-03-25 00:29:29 -0400

    Revert "usb: gadget: u_bam: Lock access to port_usb in start/stop transfers"
    This reverts commit fe2d6b2eacbfd8de48d27e45fa0c3ed76761c7b1.

--- drivers/usb/gadget/u_bam.c
+++ drivers/usb/gadget/u_bam.c
@@ -539,9 +539,7 @@ static void gbam_start_endless_rx(struct gbam_port *port)
 	struct bam_ch_info *d = &port->data_ch;
 	int status;
 
-	spin_lock(&port->port_lock_ul);
 	if (!port->port_usb) {
-		spin_unlock(&port->port_lock_ul);
 		pr_err("%s: port->port_usb is NULL", __func__);
 		return;
 	}
@@ -550,7 +548,6 @@ static void gbam_start_endless_rx(struct gbam_port *port)
 	status = usb_ep_queue(port->port_usb->out, d->rx_req, GFP_ATOMIC);
 	if (status)
 		pr_err("%s: error enqueuing transfer, %d\n", __func__, status);
-	spin_unlock(&port->port_lock_ul);
 }
 
 static void gbam_start_endless_tx(struct gbam_port *port)
@@ -558,9 +555,7 @@ static void gbam_start_endless_tx(struct gbam_port *port)
 	struct bam_ch_info *d = &port->data_ch;
 	int status;
 
-	spin_lock(&port->port_lock_dl);
 	if (!port->port_usb) {
-		spin_unlock(&port->port_lock_dl);
 		pr_err("%s: port->port_usb is NULL", __func__);
 		return;
 	}
@@ -569,8 +564,6 @@ static void gbam_start_endless_tx(struct gbam_port *port)
 	status = usb_ep_queue(port->port_usb->in, d->tx_req, GFP_ATOMIC);
 	if (status)
 		pr_err("%s: error enqueuing transfer, %d\n", __func__, status);
-	spin_unlock(&port->port_lock_dl);
-
 }
 
 static void gbam_stop_endless_rx(struct gbam_port *port)
@@ -578,9 +571,7 @@ static void gbam_stop_endless_rx(struct gbam_port *port)
 	struct bam_ch_info *d = &port->data_ch;
 	int status;
 
-	spin_lock(&port->port_lock_ul);
 	if (!port->port_usb) {
-		spin_unlock(&port->port_lock_ul);
 		pr_err("%s: port->port_usb is NULL", __func__);
 		return;
 	}
@@ -589,17 +580,14 @@ static void gbam_stop_endless_rx(struct gbam_port *port)
 	status = usb_ep_dequeue(port->port_usb->out, d->rx_req);
 	if (status)
 		pr_err("%s: error dequeuing transfer, %d\n", __func__, status);
-	spin_unlock(&port->port_lock_ul);
-}
 
+}
 static void gbam_stop_endless_tx(struct gbam_port *port)
 {
 	struct bam_ch_info *d = &port->data_ch;
 	int status;
 
-	spin_lock(&port->port_lock_dl);
 	if (!port->port_usb) {
-		spin_unlock(&port->port_lock_dl);
 		pr_err("%s: port->port_usb is NULL", __func__);
 		return;
 	}
@@ -608,7 +596,6 @@ static void gbam_stop_endless_tx(struct gbam_port *port)
 	status = usb_ep_dequeue(port->port_usb->in, d->tx_req);
 	if (status)
 		pr_err("%s: error dequeuing transfer, %d\n", __func__, status);
-	spin_unlock(&port->port_lock_dl);
 }
 
 static void gbam_start(void *param, enum usb_bam_pipe_dir dir)
