b94aac3923b277a76aa51d04d97bd0f5b0baaf30
Chet Kener <Cl3Kener@gmail.com>
2014-08-26 14:21:44 -0400

    Revert "[backport] sched_rt: Upstream fixes"
    This reverts commit 488338e3c47ac7c6e547d36519c9adce5209504b.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1242,6 +1242,7 @@ struct sched_rt_entity {
 	struct list_head run_list;
 	unsigned long timeout;
 	unsigned int time_slice;
+	int nr_cpus_allowed;
 
 	struct sched_rt_entity *back;
 #ifdef CONFIG_RT_GROUP_SCHED
@@ -1312,7 +1313,6 @@ struct task_struct {
 #endif
 
 	unsigned int policy;
-	int nr_cpus_allowed;
 	cpumask_t cpus_allowed;
 
 #ifdef CONFIG_PREEMPT_RCU
